ae3529219c92976e5d5b85ce98ea7ee31330ae43
edalonzoh90 <edalonzoh@gmail.com>
2022-02-21 18:12:51 -0600

    Added UserStory/ValidateUpdate

--- RS.api/Controllers/WorkItemController.cs
+++ RS.api/Controllers/WorkItemController.cs
@@ -25,39 +25,61 @@ namespace RS.api.Controllers
             _workItemService = workItemService;
         }
 
-        [Route("ValidateUpdate")]
+        //[Route("ValidateUpdate")]
+        //[Authorize]
+        //[HttpPost]
+        //public async Task<IActionResult> ValidateUpdate(WorkItemParam wiParam)
+        //{
+        //    string resp = "{ 'status': 0}";
+        //    if(wiParam.resource.fields.SystemState.oldValue != "Dev Review" && wiParam.resource.fields.SystemState.newValue != "Dev Review")
+        //        return new JsonResult(resp);
+
+        //    List<Relation> lst = wiParam.resource.revision.relations;
+        //    foreach (Relation rel in lst)
+        //    {
+        //        if (rel.attributes.name == "Pull Request")
+        //        {
+        //            PullRequestParam param = new PullRequestParam()
+        //            {
+        //                resource = new Resource()
+        //                {
+        //                    pullRequestId = int.Parse(rel.url.Substring((rel.url.Length - 4)))
+        //                }
+        //            };
+
+        //            bool wiStatus = await _pullRequestService.ValidateWorkItemStatusAsync(param);
+
+        //            PullRequestStatusParam prParam = new PullRequestStatusParam()
+        //            {
+        //                context = new ContextModel() { name = "Work Item State" },
+        //                state = wiStatus ? PullRequestStatusParam.SUCCESS_STATE : PullRequestStatusParam.FAIL_STATE
+        //            };
+
+        //            await _pullRequestStatusesService.SetStatusAsync(param.resource.pullRequestId, prParam);
+        //        }
+        //    }
+
+        //    return new JsonResult(resp);
+        //}
+
+        [Route("UserStory/ValidateUpdate")]
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult> ValidateUpdate(WorkItemParam wiParam)
+        public async Task<IActionResult> ValidateUpdateUS(WorkItemParam wiParam)
         {
             string resp = "{ 'status': 0}";
-            if(wiParam.resource.fields.SystemState.oldValue != "Dev Review" && wiParam.resource.fields.SystemState.newValue != "Dev Review")
+            //Verify if is Task or Bug -> This validation comes from the hook
+
+            //Verify the state of the task
+            if (wiParam.resource.fields.SystemState.newValue != "QA")
                 return new JsonResult(resp);
 
-            List<Relation> lst = wiParam.resource.revision.relations;
-            foreach (Relation rel in lst)
-            {
-                if (rel.attributes.name == "Pull Request")
-                {
-                    PullRequestParam param = new PullRequestParam()
-                    {
-                        resource = new Resource()
-                        {
-                            pullRequestId = int.Parse(rel.url.Substring((rel.url.Length - 4)))
-                        }
-                    };
-
-                    bool wiStatus = await _pullRequestService.ValidateWorkItemStatusAsync(param);
-
-                    PullRequestStatusParam prParam = new PullRequestStatusParam()
-                    {
-                        context = new ContextModel() { name = "Work Item State" },
-                        state = wiStatus ? PullRequestStatusParam.SUCCESS_STATE : PullRequestStatusParam.FAIL_STATE
-                    };
-
-                    await _pullRequestStatusesService.SetStatusAsync(param.resource.pullRequestId, prParam);
-                }
-            }
+            //Get the Parent
+            WorkItemResponse wiParent = await _workItemService.GetDetailAsync(wiParam.resource.revision.fields.SystemParent);
+            
+            //Verify if is US
+            //Get all its childs and verify its status
+            //If all are in QA and the US is not QA -> update the status to QA 
 
             return new JsonResult(resp);
         }
--- RS.api/Models/WorkItemParam.cs
+++ RS.api/Models/WorkItemParam.cs
@@ -18,6 +18,9 @@ namespace RS.api.Models
     {
         [JsonPropertyName("System.State")]
         public SystemState SystemState { get; set; }
+
+        [JsonPropertyName("System.Parent")]
+        public int SystemParent { get; set; }
     }
 
     public class Relation
@@ -35,8 +38,16 @@ namespace RS.api.Models
         public WIFields fields { get; set; }
     }
 
+    public class RevFields
+    {
+
+        [JsonPropertyName("System.Parent")]
+        public int SystemParent { get; set; }
+    }
+
     public class Revision
     {
         public List<Relation> relations { get; set; }
+        public RevFields fields { get; set; }
     }
 }
